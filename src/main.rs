4d170e3b06028cd2f66f3eff17d7c98fbaffcac4
Lucas Martins <luscagbr@gmail.com>
2022-05-10 19:30:49 -0300

    setting pid values

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,103 @@
 extern crate ev3dev_lang_rust;
 
-use ev3dev_lang_rust::motors::TachoMotor ;
+use ev3dev_lang_rust::{ Ev3Button, Ev3Result };
+use ev3dev_lang_rust::sensors::ColorSensor;
+use ev3dev_lang_rust::motors::{TachoMotor, MotorPort, LargeMotor};
 // use std::thread;
 // use std::time::Duration;
 use std::{thread, time::Duration};
 use std::env;
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    dir: i32,
+    speed: i32,
+    pos: i32,
+    p: f32,
+    i: f32,
+    d: f32
+}
     
 fn main() -> ev3dev_lang_rust::Ev3Result<()> {
     env::set_var("RUST_BACKTRACE", "1");
+    let args = Args::parse();
 
-    let motor = TachoMotor::find()?;
+    let arg_p = args.p;
+    let arg_i = args.i;
+    let arg_d = args.d;
 
-    motor.set_speed_sp(50.0 as i32)?;
-    motor.set_position_sp(100 as i32)?;
+    let motor = LargeMotor::get(MotorPort::OutA)?;
+    let motor_default = LargeMotor::get(MotorPort::OutB)?;
+
+    motor.reset()?;
+    motor_default.reset()?;
+
+    println!("Position: {}", motor.get_position()?);
+    println!("Position Default: {}", motor_default.get_position()?);
+    // println!("Travel count Final: {}", motor.get_full_travel_count()?);
+    let dir = match args.dir {
+        1 => { -1 },
+        _ => { 1 }
+    };
+
+    println!("Dir: {}", dir);
+    
+    motor.set_speed_sp(args.speed)?;
+    motor.set_position_sp(args.pos * dir)?;
+    motor.set_stop_action(TachoMotor::STOP_ACTION_HOLD)?;
+
+    motor.set_hold_pid_kp(arg_p)?;
+    motor.set_hold_pid_ki(arg_i)?;
+    motor.set_hold_pid_kd(arg_d)?;
+
+    motor.set_speed_pid_kp(arg_p)?;
+    motor.set_speed_pid_ki(arg_i)?;
+    motor.set_speed_pid_kd(arg_d)?;
+
+    motor_default.set_speed_sp(args.speed)?;
+    motor_default.set_position_sp(args.pos * dir)?;
+    motor_default.set_stop_action(TachoMotor::STOP_ACTION_HOLD)?;
 
     motor.run_to_rel_pos(None)?;
+    motor_default.run_to_rel_pos(None)?;
 
-    // thread::sleep(Duration::from_millis(2000));
-    
-    motor.set_hold_pid_kd(1.0)?;
-    motor.set_hold_pid_ki(1.0)?;
-    // let p = motor.get_hold_pid_kd()?;
-    // motor.
+    motor.wait_while(TachoMotor::STATE_RUNNING, Some(Duration::from_secs(3)));
 
+    println!("#### SPEED ####");
     let p = motor.get_speed_pid_kp()?;
     let i = motor.get_speed_pid_ki()?;
-
-    println!("{:?} {:?}", p, i);
-
-    // let i = motor.get_speed_pid_ki()?;
-    // let d = motor.get_speed_pid_kd()?;
+    let d = motor.get_speed_pid_kd()?;
+    println!("p: {:?} i: {:?} d: {:?}", p, i, d);
     
-    // println!("{} {} {}", p, i, d);
+    let p = motor_default.get_speed_pid_kp()?;
+    let i = motor_default.get_speed_pid_ki()?;
+    let d = motor_default.get_speed_pid_kd()?;
+    println!("Default p: {:?} i: {:?} d: {:?}", p, i, d);
 
-    // let p = motor.get_hold_pid_kp()?;
-    // let i = motor.get_hold_pid_ki()?;
-    // let d = motor.get_hold_pid_kd()?;
+    println!("#### HOLD ####");
+    let p = motor.get_hold_pid_kp()?;
+    let i = motor.get_hold_pid_ki()?;
+    let d = motor.get_hold_pid_kd()?;
+    println!("p: {:?} i: {:?} d: {:?}", p, i, d);
     
-    // println!("{} {} {}", p, i, d);
+    let p = motor_default.get_hold_pid_kp()?;
+    let i = motor_default.get_hold_pid_ki()?;
+    let d = motor_default.get_hold_pid_kd()?;
+    println!("Default p: {:?} i: {:?} d: {:?}", p, i, d);
 
+    //wait 2 seconds
+    
+    thread::sleep(Duration::from_secs(2));
+    println!("--------------------------------------");
 
+    println!("Position Final: {}", motor.get_position()?);
+    println!("Position Default Final: {}", motor_default.get_position()?);
+    // println!("Travel count Final: {}", motor.get_full_travel_count()?);
+
+    motor.stop()?;
     Ok(())
 }
